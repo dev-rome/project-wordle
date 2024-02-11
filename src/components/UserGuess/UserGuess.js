6c7993edaedaed8287324d6705cf2ee41c749eef
dev-rome <haynesjerome@gmail.com>
2024-02-11 11:22:54 -0500

    Pass answer as prop

--- src/components/UserGuess/UserGuess.js
+++ src/components/UserGuess/UserGuess.js
@@ -4,11 +4,11 @@ import GuessCells from "../Guess/GuessCells";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 
-function UserGuess({ userGuess }) {
+function UserGuess({ userGuess, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <GuessCells key={num} value={userGuess[num]} />
+        <GuessCells key={num} value={userGuess[num]} answer={answer} />
       ))}
     </div>
   );
