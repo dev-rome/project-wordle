1c0650cfdc9550474604e60a5e82120a95519d7a
dev-rome <haynesjerome@gmail.com>
2024-02-11 10:59:52 -0500

    Import range and num guesses to display cells for user guess

--- src/components/UserGuess/UserGuess.js
+++ src/components/UserGuess/UserGuess.js
@@ -1,12 +1,14 @@
 import React from "react";
+import GuessCells from "../Guess/GuessCells";
+
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function UserGuess({ userGuess }) {
   return (
     <div className="guess-results">
-      {userGuess.map(({ guess, id }) => (
-        <p key={id} className="guess">
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <GuessCells key={num} value={userGuess[num]} />
       ))}
     </div>
   );
